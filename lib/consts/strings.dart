f844b2d390ded31cd6a0897d3970e9254ab7f744
preanto <preanto15-5059@diu.edu.bd>
2025-03-26 13:48:34 +0600

    Home, Login and Signup screen

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,3 +1,20 @@
-const appname = "eMart";
+import 'package:emart_app/consts/consts.dart';
+
+const appname = "Beyond Nilkhet";
 const appversion = "Version 1.0.0";
-const credits = "@Baaba Devs";
+const credits = "@Team Alone";
+const email = "Email";
+const emailHint = "admin@admin.com";
+const password = "Password";
+const passwordHint = "******";
+const retypePassword = "Retype Password";
+const name = "Name";
+const nameHint = "John Doe";
+const forgetpass = 'Forget Password';
+const login = "Login";
+const signup = "Sign up";
+const createNewAccount = "or, create a new account";
+const loginWith = "Log in With";
+const privacyPolicy = "Privacy Policy";
+const termsandcond = "Terms and Conditions";
+const alreadyHaveAccount = "Already Have an Account?";
---
+++ lib/views/auth_screen/signup_screen.dart
@@ -0,0 +1,118 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/applogo.widget.dart';
+import 'package:emart_app/widgets_common/bg_widget.dart';
+import 'package:emart_app/widgets_common/custom_textfield.dart';
+import 'package:emart_app/widgets_common/our_button.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            applogoWidget(),
+            10.heightBox,
+            "Create and Account at $appname"
+                .text
+                .fontFamily(bold)
+                .white
+                .size(18)
+                .make(),
+            10.heightBox,
+            Column(
+              children: [
+                customTextField(hint: nameHint, title: name),
+                customTextField(hint: emailHint, title: email),
+                customTextField(hint: passwordHint, title: password),
+                customTextField(hint: passwordHint, title: retypePassword),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {}, child: forgetpass.text.make())),
+                5.heightBox,
+                //ourButton().box.width(context.screenWidth - 50).make(),
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(EdgeInsets.all(16))
+                .width(context.screenWidth)
+                .make(),
+            5.heightBox,
+            // createNewAccount.text.color(fontGrey).make(),
+            // 5.heightBox,
+            // ourButton(
+            //         color: golden,
+            //         title: signup,
+            //         textColor: redColor,
+            //         onPress: () {})
+            //     .box
+            //     .width(context.screenWidth - 50)
+            //     .make(),
+            Row(
+              children: [
+                Checkbox(
+                    checkColor: redColor,
+                    value: false,
+                    onChanged: (newValue) {}),
+                10.widthBox,
+                Expanded(
+                    child: RichText(
+                        text: const TextSpan(children: [
+                  TextSpan(
+                      text: "I agree to the ",
+                      style: TextStyle(
+                        fontFamily: bold,
+                        color: fontGrey,
+                      )),
+                  TextSpan(
+                      text: termsandcond,
+                      style: TextStyle(
+                        fontFamily: bold,
+                        color: redColor,
+                      )),
+                  TextSpan(
+                      text: privacyPolicy,
+                      style: TextStyle(
+                        fontFamily: bold,
+                        color: redColor,
+                      )),
+                ])))
+              ],
+            ),
+            ourButton(
+                    color: redColor,
+                    title: signup,
+                    textColor: whiteColor,
+                    onPress: () {})
+                .box
+                .width(context.screenWidth - 50)
+                .make(),
+            RichText(
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                    text: alreadyHaveAccount,
+                    style: TextStyle(fontFamily: bold, color: fontGrey),
+                  ),
+                  TextSpan(
+                    text: login,
+                    style: TextStyle(fontFamily: bold, color: redColor),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
